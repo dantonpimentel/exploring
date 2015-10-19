79be1833c178b2f363755ffdf68b7dcda55b547b
Danton Pimentel <dantonpimentel@gmail.com>
2015-10-18 21:50:19 -0400

    Adding decorator test

--- functions.py
+++ functions.py
@@ -1,36 +1,64 @@
 def a(a, *b):
     return a+sum(b)
 
+
 def b(a, b):
     pass
+
 
 def c(a, *b, **c):
     pass
 
+
 #def add(a:int, b:int) -> int:
 #    return(a+b)
 
+
 def foo(bar):
-    def inception(*a,**b):
+    def inception(*a, **b):
         print('-----')
         print(bar)
         print(a)
         print(b)
     return inception
 
-if __name__ == '__main__':
-    print(a(1,2,3,45,6))
 
-    print(b(1,b=2))
+def test_functions():
+    print(a(1, 2, 3, 45, 6))
 
-    print(c(1,2,c=3))
+    print(b(1, b=2))
+
+    print(c(1, 2, c=3))
 
     #print((add))
 
-    z = {1:['a',2,'s',6]}
-    w = ['b',56,5.4,3]
+    z = {1:['a', 2, 's', 6]}
+    w = ['b', 56, 5.4, 3]
     x = foo(z)
     y = foo(w)
 
-    x(12,4,4,5,6,4,x=1,y=3,t=5)
-    y(647839,y=4)
+    x(12, 4, 4, 5, 6, 4, x=1, y=3, t=5)
+    y(647839, y=4)
+
+
+def print_params(func):
+    def print_values(*args, **kwargs):
+        print(func.__name__ + '(' +
+              ', '.join([str(arg) for arg in args]) +
+              (', ' if args and kwargs else '') +
+              ', '.join(["{}={}".format(k, v) for k, v in kwargs.items()]) + ')')
+        return func(*args, **kwargs)
+    return print_values
+
+
+@print_params
+def a(x, y, z=0):
+    return x + y + z
+
+
+def test_decorator():
+    print(a(1, 2, 3))
+
+
+if __name__ == '__main__':
+    test_decorator()
